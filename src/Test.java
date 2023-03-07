cb70dbde1335124686187e522568b4706b59da6a
bigbigbird <160849360@qq.com>
2023-03-07 12:37:20 +0800

    第一次提交

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+/**
+ * ClassName: Test
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author 无涯
+ * @Create 2023/3/7 12:32
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
